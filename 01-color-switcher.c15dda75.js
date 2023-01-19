140bac1dc91596a7e17a33a053e63c93ada0de2a
ErikaTatchyn <94489330+ErikaTatchyn@users.noreply.github.com>
2023-01-19 14:47:55 +0000

    Deploying to gh-pages from @ ErikaTatchyn/goit-js-hw-09@f620b3489833c3c2414de68f3815a5b97e64f475 🚀

--- 01-color-switcher.c15dda75.js
+++
@@ -1,2 +0,0 @@
-function t(){document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}const e=document.querySelector("[data-stop]"),o=document.querySelector("[data-start]");let d;e.disabled=!0,o.onclick=function(){d=setInterval(t,1e3),this.disabled=!0,e.disabled=!1},e.onclick=function(){clearInterval(d),this.disabled=!0,o.disabled=!1};
-//# sourceMappingURL=01-color-switcher.c15dda75.js.map
